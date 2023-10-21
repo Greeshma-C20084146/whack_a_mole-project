4a2e7bbe29c5281d266f3c30752f6ecc93a7c1a9
girishraja1508 <148402050+girishraja1508@users.noreply.github.com>
2023-10-20 20:36:33 -0400

    Add files via upload
    uploading svg files and java script file

---
+++ script1.js
@@ -0,0 +1,57 @@
+const h = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+  const moles = document.querySelectorAll('.mole');
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+
+  function startGame() {
+    score = 0;
+    scoreBoard.textContent = score;
+    timeUp = false;
+    peep();
+    setTimeout(() => {
+      timeUp = true;
+
+      setTimeout(() => {
+        const finalScoreValue = document.getElementById('final_score_value');
+        finalScoreValue.textContent = score;
+  
+        const finalScoreInterface = document.querySelector('.final-score');
+        finalScoreInterface.style.display = 'block';
+      }, 1000); 
+    }, 10000);
+  }
+
+  function randomTime(min, max) { 
+    return Math.round(Math.random() * (max - min) +2* min);
+    const time = randomTime(50, 1000);
+  }
+
+  function whack(e) {
+    if(!e.isTrusted) return; 
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+  }
+  function peep() {
+    const time = randomTime(200, 1000);
+    const hole = random_Hole(h);
+    hole.classList.add('up');
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+  function random_Hole(h) {
+    const idx = Math.floor(Math.random() * h.length);
+    const hole = h[idx];
+    if (hole === lastHole) {
+      return random_Hole(h);
+    }
+    lastHole = hole;
+    return hole;
+  }
+
+
+  moles.forEach(mole => mole.addEventListener('click', whack));
