700d7524c134d38f92ccafa9d9a8e22309fb1fd5
girishraja1508 <148402050+girishraja1508@users.noreply.github.com>
2023-10-25 22:22:21 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,28 +1,45 @@
 //Greeshma Chandra Shekar - C20084146; Girish Raja Gunda - C13275928; Apoorva Gaddam - C55159095
 
-  const h = document.querySelectorAll('.hole');
+   const h = document.querySelectorAll('.hole');
   const scoreBoard = document.querySelector('.score');
+  const finalScoreValue = document.getElementById('final_score_value');
+  const finalScoreInterface = document.querySelector('.final-score');
   const moles = document.querySelectorAll('.mole');
+  const level = document.getElementById('game_level');
+  const htmlElement = document.documentElement
   let lastHole;
-  let timeUp = false;
+  let timeUp = true;
   let score = 0;
+  let max_time = 1000;
+  
+  
+  function endGame()
+  {
+    timeUp = true;
+    finalScoreValue.textContent = score;
+    finalScoreInterface.style.display = 'block';
+  }
 
   function startGame() {
-    score = 0;
-    scoreBoard.textContent = score;
-    timeUp = false;
-    peep();
-    setTimeout(() => {
-      timeUp = true;
+    if (timeUp) {
+      score = 0;
+      scoreBoard.textContent = score;
+      timeUp = false;
+      level.innerHTML = 'Level Easy';
+      htmlElement.style.backgroundColor = '#a9f23a';
+      peep();
+     setTimeout(() => {
+        timeUp = true;
 
-      setTimeout(() => {
-        const finalScoreValue = document.getElementById('final_score_value');
-        finalScoreValue.textContent = score;
-  
-        const finalScoreInterface = document.querySelector('.final-score');
-        finalScoreInterface.style.display = 'block';
-      }, 1000); 
-    }, 600000);
+       //setTimeout(() => {
+          
+          finalScoreValue.textContent = score;
+    
+          
+          finalScoreInterface.style.display = 'block';
+      // }, 1000); 
+      }, 600000);
+    }
   }
 
   function randomTime(min, max) { 
@@ -35,10 +52,24 @@
     score++;
     this.parentNode.classList.remove('up');
     scoreBoard.textContent = score;
+    if (score>20 & score<=50){
+      level.innerHTML="Level 2"
+      htmlElement.style.backgroundColor = "#f2c13a";
+      max_time = 500
+    }else if(score>50){
+      level.innerHTML="Level 3"
+      htmlElement.style.backgroundColor = "#f25f3a";
+      max_time = 300
+    }
+    else if(score<=20){
+      level.innerHTML="Level 1"
+      htmlElement.style.backgroundColor = "#a9f23a";
+      max_time = 1000
+    }
   }
 function peep() {
   if (!timeUp) {
-    const time = randomTime(200, 1000);
+    const time = randomTime(200, max_time);
     const hole = random_Hole(h);
 
     if (hole.classList.contains('up')) {
@@ -48,12 +79,13 @@ function peep() {
 
       setTimeout(() => {
         hole.classList.remove('up');
-        if (!timeUp) peep();
+        peep();
       }, time);
     }
   }
 }
   function random_Hole(h) {
+    if (!timeUp) {
     const idx = Math.floor(Math.random() * h.length);
     const hole = h[idx];
     if (hole === lastHole) {
@@ -62,6 +94,11 @@ function peep() {
     lastHole = hole;
     return hole;
   }
+}
 
 
   moles.forEach(mole => mole.addEventListener('click', whack));
+  document.getElementById('startGameButton').addEventListener('click', startGame);
+
+  
+  document.getElementById('endGameButton').addEventListener('click', endGame);
